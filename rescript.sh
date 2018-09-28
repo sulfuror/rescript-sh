60754e7433b204db1b1c852d5da022af51fd71b0
Sulfuror <sulfuror@gmail.com>
2018-09-28 00:12:04 +0000

    Update rescript.sh

--- rescript.sh
+++ rescript.sh
@@ -10,18 +10,27 @@
 # ------------------------------------------------------------------------------- #
 #                              R E P O    I N F O                                 #
 # ------------------------------------------------------------------------------- #
-
+# You MUST set these values. "KEEP" values CANNOT be left blank; if you are not
+# going to use "KEEP" policies the value must be set to "0".
 RESTIC_PASSWORD="CHANGE_ME"
 RESTIC_REPO="/PATH/TO/YOUR/REPO"
 BACKUP_DIR="$HOME"
-DESTINATION="Local Backup"
-TAG=""
 KEEP_HOURLY="8"
 KEEP_DAILY="7"
 KEEP_WEEKLY="4"
 KEEP_MONTHLY="12"
 KEEP_YEARLY="10"
+
+# Optional values; The "DESTINATION" value is just use in this script to display
+# in output the name of your destination (e.g.: Google Drive, Amazon, B2, Wasabi, 
+# etc.). The "TAG" value will tag your snapshots with any name/tag you want.
+# The "CLEAN" value determine when it will execute the forget, prune and check
+# commands; this value must be set in days and by default the number 7 means
+# it will do these tasks every seven (7) days. These values CAN be left blank
+# if you are not planning on use them.
 CLEAN="7"
+TAG=""
+DESTINATION=""
 
 # ------------------------------------------------------------------------------- #
 #                                 E X C L U D E S                                 #
@@ -48,7 +57,7 @@ EXCLUDE13=""
 EXCLUDE14=""
 EXCLUDE15=""
 
-# AFTER THIS LINE YOU DON'T REALLY NEED TO DO ANYTHING ELSE, YOU'RE DONE NOW
+# AFTER THIS LINE YOU DO NOT NEED TO DO ANYTHING ELSE, YOU'RE DONE NOW
 # PUT THE SCRIPT TO WORK (remember to give the right to execute with 
 # 'chmod 700 rescript.sh') AND GO BACK TO YOUR LIFE
 # =============================================================================== #
@@ -64,6 +73,16 @@ SECONDS=0
 export RESTIC_PASSWORD=$RESTIC_PASSWORD
 export RESTIC_REPOSITORY=$RESTIC_REPO
 
+# Set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# Set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
 # Create a rescript directories
 if [[ ! -d "$HOME/.rescript" ]]; then
   mkdir -p "$HOME/.rescript"
@@ -76,15 +95,17 @@ if [[ ! -d "$HOME/.rescript/lock" ]]; then
 fi
 
 # If there is no date file create one
-if [[ ! -e "$DATEFILE" ]]; then
-  touch "$DATEFILE"
-  date -R > "$DATEFILE"
+if [[ ! -z "$CLEAN" ]] ; then
+  if [[ ! -e "$DATEFILE" ]]; then
+    touch "$DATEFILE"
+    date -R > "$DATEFILE"
+  fi
 fi
 # ------------------------------------------------------------------------------- #
-Version="rescript.sh-v1.3"
+Version="rescript.sh-v1.4"
 Usage='Author		: Sulfuror, Copyright (c) 2018 <sulfuror@gmail.com>
 License		: BSD 2-clause "Simplified" License
-Version		: rescript.sh-v1.3
+Version		: rescript.sh-v1.4
 Description	: rescript.sh is a shell script created to manage
 		  backups made with restic program.
 
@@ -116,20 +137,23 @@ Automatic Options:
 	-h, -help		Help for this script
 	-m, -mount		Mount your restic repo
 	-n, -next-cleanup	Display when it will do the next cleanup
-	-r, -restore		Restore latest snapshot
 	-v, -version		Version of this script
 	-s, -stats		Stats [original and deduplicated size]
 	-u, -unlock		Remove lock created by script
 
-User options [all user options require arguments]:
+User options [all user options require an argument]:
 	-f			forget: Remove snapshots from the repository
 	-g			find: Find a file or directory
-	-k			keys: Manage keys [passwords]
+	-k			keys: Manage keys [list|add|passwd]
 	-l			ls: List files in a snapshot	
+	-r			Restore a snapshot
+
+For more information about the usage check out the following link:
+https://gitlab.com/sulfuror/rescript.sh/blob/master/README.md#usage"
 '
 # ------------------------------------------------------------------------------- #
 # Options
-while getopts ":bcdf:g:hk:l:mnrsuv" o
+while getopts ":bcdf:g:hk:l:mnr:suv" o
   do
       case "$o" in
 	b|backup)
@@ -173,24 +197,41 @@ while getopts ":bcdf:g:hk:l:mnrsuv" o
 		fi
 		touch "$LOCK"
 		trap 'rm -rf "$LOCK"' INT QUIT TERM EXIT
-		echo -e "======================================================================"
-		echo -e "|- - - - - - - > [ S T A R T I N G    C L E A N U P ] < - - - - - - -|"
-		echo -e "======================================================================"
-		echo -e "$YELLOW""Start:""$ENDCOLOR" "$(date)"
-		echo -e "----------------------------------------------------------------------"
-		echo -e "$YELLOW""Cleaning up $DESTINATION...""$ENDCOLOR"
-	        restic forget --keep-hourly $KEEP_HOURLY --keep-daily $KEEP_DAILY --keep-weekly $KEEP_WEEKLY --keep-monthly $KEEP_MONTHLY --keep-yearly $KEEP_YEARLY
-	        restic prune --cleanup-cache
-		NOW=$(date +"%s")
-		NEXT=$(date -f "$DATEFILE" "+%s")
-		RESULT=$((NEXT-NOW))
-		echo -e "$YELLOW""Next check and cleanup in "$((RESULT / 86400 ))" days "$(((RESULT / 3600) % 24))" hours and "$(((RESULT / 60) % 60))" minutes...""$ENDCOLOR"
-		echo -e "----------------------------------------------------------------------"
-		echo -e "$YELLOW""End:""$ENDCOLOR" "$(date)" "         " "$YELLOW""Duration:""$ENDCOLOR" "$((SECONDS / 3600))hrs $(((SECONDS / 60) % 60))min $((SECONDS % 60))sec"
-		echo -e "======================================================================"
-		echo -e "| - - - - - - - - > [ C L E A N U P    E N D E D ] < - - - - - - - - |"
-		echo -e "======================================================================"
-	 	   ;;
+		if [[ "$KEEP_HOURLY" -gt "0" || "$KEEP_DAILY" -gt "0" || "$KEEP_WEEKLY" -gt "0" || "$KEEP_MONTHLY" -gt "0" || "$KEEP_YEARLY" -gt "0" ]] ; then
+			echo -e "======================================================================"
+	  		echo -e "|- - - - - - - > [ S T A R T I N G    C L E A N U P ] < - - - - - - -|"
+			echo -e "======================================================================"
+			echo -e "$YELLOW""Start:""$ENDCOLOR" "$(date)"
+			echo -e "----------------------------------------------------------------------"
+			echo -e "$YELLOW""Cleaning up $DESTINATION...""$ENDCOLOR"
+		        restic forget --keep-hourly $KEEP_HOURLY --keep-daily $KEEP_DAILY --keep-weekly $KEEP_WEEKLY --keep-monthly $KEEP_MONTHLY --keep-yearly $KEEP_YEARLY
+		        restic prune --cleanup-cache
+			NOW=$(date +"%s")
+			NEXT=$(date -f "$DATEFILE" "+%s")
+			RESULT=$((NEXT-NOW))
+			if test "$CLEAN" -gt "0" ; then
+			  if test "$RESULT" -lt "0" ; then
+			    echo -e "$YELLOW""Repo will be checked and cleaned in the next run...""$ENDCOLOR"
+			  else
+			    if test "$((RESULT / 86400))" -lt "2" ; then
+			      echo -e "$YELLOW""Next check and cleanup in "$((RESULT / 86400 ))" day "$(((RESULT / 3600) % 24))" hours and "$(((RESULT / 60) % 60))" minutes...""$ENDCOLOR"
+			    else
+			      echo -e "$YELLOW""Next check and cleanup in "$((RESULT / 86400 ))" days "$(((RESULT / 3600) % 24))" hours and "$(((RESULT / 60) % 60))" minutes...""$ENDCOLOR"
+			    fi
+			  fi
+			fi
+			echo -e "----------------------------------------------------------------------"
+			echo -e "$YELLOW""End:""$ENDCOLOR" "$(date)" "         " "$YELLOW""Duration:""$ENDCOLOR" "$((SECONDS / 3600))hrs $(((SECONDS / 60) % 60))min $((SECONDS % 60))sec"
+			echo -e "======================================================================"
+			echo -e "| - - - - - - - - > [ C L E A N U P    E N D E D ] < - - - - - - - - |"
+			echo -e "======================================================================"
+		else
+			echo -e "$YELLOW""You have not indicated any policy value...""$ENDCOLOR"
+			echo "If you want to use -c, -cleanup option you need to set the [KEEP] variables."
+			echo "For more information about the Usage check out the following link:"
+			echo "https://gitlab.com/sulfuror/rescript.sh/blob/master/README.md#usage"
+		fi
+		;;
 	d|deep-check)
 		if [ -e "$LOCK" ]; then
 		  echo -e "$YELLOW""WARNING:""$ENDCOLOR" "$(basename "$0") is already running..."
@@ -295,12 +336,32 @@ while getopts ":bcdf:g:hk:l:mnrsuv" o
 		rm -rf "$rmount"
 		;;
 	n|next-cleanup)
+		if [[ -z "$CLEAN" || "$CLEAN" -lt "1" ]] ; then
+		  echo -e "$YELLOW""You have not indicated any policy for the CLEAN value...""$ENDCOLOR"
+		  echo "The scrip will run check, forget and prune every time it runs"
+		  echo "unless you change the CLEAN variable at the beginning of this script."
+		  echo "The number indicated in the CLEAN variable must be in days."
+		  echo "For more information about the usage check out the following link:"
+		  echo "https://gitlab.com/sulfuror/rescript.sh/blob/master/README.md#usage"
+		else
 		NOW=$(date +"%s")
 		NEXT=$(date -f "$DATEFILE" "+%s")
 		RESULT=$((NEXT-NOW))
-		echo -e "$YELLOW""Next check and cleanup in "$((RESULT / 86400 ))" days "$(((RESULT / 3600) % 24))" hours and "$(((RESULT / 60) % 60))" minutes...""$ENDCOLOR"
+		DAYS=$((RESULT / 86400))
+		HOURS=$(((RESULT / 3600) % 24))
+		MINUTES=$(((RESULT / 60) % 60))
+		  if [[ "$DAYS" -gt "0" ]] ; then
+		    echo -e "$YELLOW""Next check and cleanup in $DAYS days $HOURS hours and $MINUTES minutes...""$ENDCOLOR"
+		  elif [[ "$HOURS" -gt "0" ]] ; then
+		    echo -e "$YELLOW""Next check and cleanup in $HOURS hours and $MINUTES minutes...""$ENDCOLOR"
+		  elif [[ "$MINUTES" -gt "0" ]] ; then
+		    echo -e "$YELLOW""Next check and cleanup in $MINUTES minutes...""$ENDCOLOR"
+		  else
+		    echo -e "$YELLOW""Repo will be checked and cleaned in the next run...""$ENDCOLOR"
+		  fi
+		fi
 		;;
-	r|restore)
+	r)
 		if [ -e "$LOCK" ]; then
 		  echo -e "$YELLOW""WARNING:""$ENDCOLOR" "$(basename "$0") is already running..."
 		  echo -e "If you are sure $(basename "$0") is not running, type"
@@ -314,15 +375,18 @@ while getopts ":bcdf:g:hk:l:mnrsuv" o
 		fi
 		touch "$LOCK"
 		trap 'rm -rf "$LOCK"' INT QUIT TERM EXIT
-		restic_restore="$HOME/$(basename "$0")-restore-$(date +%s)"
+		if [ -z "$OPTARG" ] ; then
+		  echo "You need to indicate which snapshot you want to restore..."
+		fi
+		restic_restore="$HOME/restic-restore-ID-$OPTARG"
 		mkdir "$restic_restore"
 		echo -e "======================================================================"
 		echo -e "|- - - - - - - > [ S T A R T I N G    R E S T O R E ] < - - - - - - -|"
 		echo -e "======================================================================"
 		echo -e "$YELLOW""Start:""$ENDCOLOR" "$(date)"
 		echo -e "----------------------------------------------------------------------"
-		echo -e "$YELLOW""Restoring backup from $DESTINATION...""$ENDCOLOR"
-		restic restore latest --target "$restic_restore"
+		echo -e "$YELLOW""Restoring $OPTARG snapshot from $DESTINATION...""$ENDCOLOR"
+		restic restore "$OPTARG" --verify --target "$restic_restore"
 		echo -e "----------------------------------------------------------------------"
 		echo -e "$YELLOW""End:""$ENDCOLOR" "$(date)" "         " "$YELLOW""Duration:""$ENDCOLOR" "$((SECONDS / 3600))hrs $(((SECONDS / 60) % 60))min $((SECONDS % 60))sec"
 		echo -e "======================================================================"
@@ -363,14 +427,17 @@ while getopts ":bcdf:g:hk:l:mnrsuv" o
 			echo -e "$YELLOW""Script unlocked...""$ENDCOLOR"
 		fi
 		;;
-	?)
+	\?)
 	        echo "Unknown option $OPTARG"
+		echo "$Usage"
 	        ;;
 	:)
-	        echo "No argument value for option $OPTARG"
+		echo "No argument value was indicated for option $OPTARG"
+		echo "$Usage"
 	        ;;
 	*)
 	        echo "Unknown error while processing options"
+		echo "$Usage"
 	        ;;
       esac
     exit
@@ -497,23 +564,49 @@ echo -e "$YELLOW""[Snapshots List...]""$ENDCOLOR"
 restic snapshots
 
 # Check and Clean Repo Based on User's Policy
-NOW=$(date +"%s")
-NEXT=$(date -f "$DATEFILE" "+%s")
-RESULT=$((NEXT-NOW))
-
-if test -f "$DATEFILE" ; then
-  if test "$NOW" -lt "$NEXT" ; then     
-     echo -e "$YELLOW""[Repo will be checked and cleaned in "$((RESULT / 86400 ))" days "$(((RESULT / 3600) % 24))" hours and "$(((RESULT / 60) % 60))" minutes...]""$ENDCOLOR"
-  else
-     echo -e "$YELLOW""[Checking for Errors in Repo...]""$ENDCOLOR"
-     restic check
-     echo -e "$YELLOW""[Cleaning Repo...]""$ENDCOLOR"
-     restic forget --keep-hourly $KEEP_HOURLY --keep-daily $KEEP_DAILY --keep-weekly $KEEP_WEEKLY --keep-monthly $KEEP_MONTHLY --keep-yearly $KEEP_YEARLY
-     restic prune --cleanup-cache
-     date -d now+"$CLEAN"days > "$DATEFILE"
-     echo -e "$YELLOW""[Done Cleaning; Next Cleaning Will Be Done in $CLEAN days...]""$ENDCOLOR"
+if [[ -f "$DATEFILE" || "$CLEAN" -gt "0" ]]; then
+  NOW=$(date +"%s")
+  NEXT=$(date -f "$DATEFILE" "+%s")
+  RESULT=$((NEXT-NOW))
+  DAYS=$((RESULT / 86400))
+  HOURS=$(((RESULT / 3600) % 24))
+  MINUTES=$(((RESULT / 60) % 60))
+  if test "$NOW" -lt "$NEXT" ; then
+    if [[ "$DAYS" -gt "0" ]] ; then
+      echo -e "$YELLOW""[Next check and cleanup in $DAYS days $HOURS hours and $MINUTES minutes...]""$ENDCOLOR"
+    elif [[ "$HOURS" -gt "0" ]] ; then
+      echo -e "$YELLOW""[Next check and cleanup in $HOURS hours and $MINUTES minutes...]""$ENDCOLOR"
+    elif [[ "$MINUTES" -gt "0" ]] ; then
+      echo -e "$YELLOW""[Next check and cleanup in $MINUTES minutes...]""$ENDCOLOR"
+    else
+      echo -e "$YELLOW""[Repo will be checked and cleaned in the next run...]""$ENDCOLOR"
+    fi
+  else 
+    if [[ "$KEEP_HOURLY" -gt "0" || "$KEEP_DAILY" -gt "0" || "$KEEP_WEEKLY" -gt "0" || "$KEEP_MONTHLY" -gt "0" || "$KEEP_YEARLY" -gt "0" ]] ; then
+      echo -e "$YELLOW""[Cleaning Repo...]""$ENDCOLOR"
+      restic forget --keep-hourly $KEEP_HOURLY --keep-daily $KEEP_DAILY --keep-weekly $KEEP_WEEKLY --keep-monthly $KEEP_MONTHLY --keep-yearly $KEEP_YEARLY
+      restic prune
+      echo -e "$YELLOW""[Checking for Errors in Repo...]""$ENDCOLOR"
+      restic check --cleanup-cache
+      if [[ "$CLEAN" -gt "0" ]] ; then
+        echo -e "$YELLOW""[Done Cleaning; Next Check and Cleanup Will Be Done in $CLEAN days...]""$ENDCOLOR"
+      fi
+    fi
   fi
+else 
+    if [[ "$KEEP_HOURLY" -gt "0" || "$KEEP_DAILY" -gt "0" || "$KEEP_WEEKLY" -gt "0" || "$KEEP_MONTHLY" -gt "0" || "$KEEP_YEARLY" -gt "0" ]] ; then
+      echo -e "$YELLOW""[Cleaning Repo...]""$ENDCOLOR"
+      restic forget --keep-hourly $KEEP_HOURLY --keep-daily $KEEP_DAILY --keep-weekly $KEEP_WEEKLY --keep-monthly $KEEP_MONTHLY --keep-yearly $KEEP_YEARLY
+      restic prune
+      echo -e "$YELLOW""[Checking for Errors in Repo...]""$ENDCOLOR"
+      restic check --cleanup-cache
+    fi
 fi
+
+if [[ -f "$DATEFILE" ]] ; then
+  date -d now+"$CLEAN"days > "$DATEFILE"
+fi
+
 
 # Stats
 echo -e "----------------------------------------------------------------------"
