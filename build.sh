8c1b70b01846158bd530c938cdc853e84c8cb458
sulfuror <sulfuror@gmail.com>
2026-07-10 17:44:14 -0400

    Testing v5.2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Script to compile rescript v5.1 from the modules in src/
+# Script to compile rescript from the modules in src/
 
 echo "Compiling rescript..."
 cat src/01_globals.sh src/02_utils.sh src/03_config.sh src/04_install.sh src/05_help.sh src/06_core.sh src/07_commands.sh src/08_main.sh > rescript.new
